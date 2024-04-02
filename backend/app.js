6408a1b6ac8e5c0f4c37da49e48dacef959ff51d
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-02 17:40:03 -0500

    Created a new repo with the skeleton files for the backend and some for the frontend

---
+++ backend/app.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+
+const express = require("express");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// compression
+// helmet
+
+const memoRouter = require("./routes/memoRoutes");
+
+const dev_db_url = "";
+mongoose.set("strictQuery", false);
+const mongoDb = process.env.MONGODB_URI_MEMOBOARD || dev_db_url;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+require("./helpers/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", memoRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err,
+  });
+});
+
+module.exports = app;
