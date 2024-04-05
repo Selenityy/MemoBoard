acc382d9146e1ad49f6eb5264b90bc60e22c1df9
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-05 15:27:56 -0500

    Added a user Id to the tags for authentication in tagRoutes

--- backend/models/tagModel.js
+++ backend/models/tagModel.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const TagSchema = new Schema({
   name: { type: String, minLength: 2, maxLenght: 20 },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 // virtual url to add more tags
