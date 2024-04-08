83972fadd5c6eee4fa0995ff575c38941121c5c3
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-08 16:06:03 -0500

    Cleaned up code

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -9,7 +9,6 @@ const dashboardController = require("../controllers/dashboardController");
 // Apply JWT authentication middleware to all routes in this router
 router.use(passport.authenticate("jwt", { session: false }));
 
-// GET
 router.get(
   "/dashboard",
   passport.authenticate("jwt", { session: false }),
@@ -17,27 +16,17 @@ router.get(
     res.json({ message: "Successful login to protected routes" });
   }
 );
-
 router.get("/data", dashboardController.userData);
-
 router.get("/timezone", dashboardController.timezone);
-
-// PUT
 router.put("/:userId/updateEmail", dashboardController.updateEmail);
-
 router.put("/:userId/updateName", dashboardController.updateName);
-
 router.put("/:userId/updateUsername", dashboardController.updateUsername);
-
 router.put("/:userId/updateTimezone", dashboardController.updateTimezone);
-
 router.put(
   "/profile",
   passport.authenticate("jwt", { session: false }),
   dashboardController.updateProfileGoogleAuth
 );
-
-// DELETE
 router.delete("/:userId/account", dashboardController.deleteAccount);
 
 router.use(
@@ -45,7 +34,6 @@ router.use(
   passport.authenticate("jwt", { session: false }),
   memoRoutes
 );
-
 router.use(
   "/tags",
   passport.authenticate("jwt", { session: false }),
