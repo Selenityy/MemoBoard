8684e0687d27e5a346c2a1e160ff93afec3aa3f7
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-10 14:21:58 -0500

    Created a sign up testing suit

---
+++ backend/__tests__/signup.test.js
@@ -0,0 +1,65 @@
+const request = require("supertest");
+const createTestApp = require("./testApp");
+const db = require("./mongoConfigTesting");
+
+beforeAll(async () => {
+  await db.startMongoServer();
+});
+
+beforeEach(async () => {
+  await db.clearMongoServer();
+});
+
+afterAll(async () => {
+  await db.stopMongoServer();
+});
+
+test("should sign up a new user successfully", async () => {
+  const app = await createTestApp();
+  const res = await request(app).post("/auth/signup").send({
+    username: "jestTest",
+    password: "jest",
+    email: "jestTest@example.com",
+    firstName: "Jest",
+    lastName: "Test",
+    timezone: "CT",
+  });
+  expect(res.statusCode).toEqual(201);
+  expect(res.body.message).toEqual("User created successfully");
+});
+
+test("should reject username for already existing", async () => {
+  const app = await createTestApp();
+  const res = await request(app).post("/auth/signup").send({
+    username: "jestTest",
+    password: "jest",
+    email: "jestTest@example.com",
+    firstName: "Jest",
+    lastName: "Test",
+    timezone: "CT",
+  });
+  const res2 = await request(app).post("/auth/signup").send({
+    username: "jestTest",
+    password: "jest",
+    email: "jestTest@example.com",
+    firstName: "Jest",
+    lastName: "Test",
+    timezone: "CT",
+  });
+  expect(res2.statusCode).toEqual(400);
+  expect(res2.body.message).toEqual("Username or email already exists");
+});
+
+test("should error with email and password required", async () => {
+  const app = await createTestApp();
+  const res = await request(app).post("/auth/signup").send({
+    username: "jestTest",
+    password: "",
+    email: "",
+    firstName: "Jest",
+    lastName: "Test",
+    timezone: "CT",
+  });
+  expect(res.statusCode).toEqual(400);
+  expect(res.body.message).toEqual("Email and password are requried");
+});
