486f5e2d86470b796ad37ab12f27ead47f5c38a1
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-10 14:22:09 -0500

    Created a test app set up file for jest

---
+++ backend/__tests__/testApp.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("../routes/authRoutes");
+const dashboardRoutes = require("../routes/dashboardRoutes");
+
+const createTestApp = () => {
+  const app = express();
+  app.use(express.json());
+
+  //   app.use(cors());
+  //   app.use(logger("dev"));
+  //   app.use(express.json());
+  //   app.use(express.urlencoded({ extended: false }));
+  //   app.use(cookieParser());
+
+  //   app.use(express.static(path.join(__dirname, "public")));
+
+  //   app.use(
+  //     session({
+  //       secret: process.env.SESSION_SECRET,
+  //       resave: false,
+  //       saveUninitialized: false,
+  //     })
+  //   );
+
+  //   require("../helpers/passport");
+  //   app.use(passport.initialize());
+  //   app.use(passport.session());
+
+  app.use("/auth", authRoutes);
+  app.use("/dashboard", dashboardRoutes);
+
+  //   app.use((req, res, next) => {
+  //     next(createError(404));
+  //   });
+
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ error: err.message });
+  });
+
+  //   app.use((err, req, res, next) => {
+  //     res.status(err.status || 500).json({
+  //       message: err.message || "An error occurred",
+  //       error: req.app.get("env") === "development" ? err : {},
+  //     });
+  //   });
+
+  return app;
+};
+
+module.exports = createTestApp;
