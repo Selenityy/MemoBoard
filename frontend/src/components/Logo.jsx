6f7fcfd3216f05b30f3d95c030597d0f35a41703
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-15 17:16:06 -0500

    Created a Logo component that holds the logo and brand name

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Row, Col } from "react-bootstrap";
+import "../styles/custom.scss";
+
+const Logo = () => {
+  return (
+    <Row className="align-items-center gx-1 gx-sm-2">
+      <Col xs="auto">
+        <div className="logo-container">
+          <Image
+            src="/images/logo2.png"
+            width={50}
+            height={50}
+            alt="Logo of MemoBoard"
+            className="img-fluid"
+          />
+        </div>
+      </Col>
+      <Col>
+        <h1 className="logo-title">MemoBoard</h1>
+      </Col>
+    </Row>
+  );
+};
+
+export default Logo;
