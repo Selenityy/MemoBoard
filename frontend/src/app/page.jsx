47b5c532f36f124cc92705f0958f2c252dbb3807
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-15 17:59:59 -0500

    Wrapped the h1 and h3 into a row, then the login form in a row

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,16 +1,26 @@
 import Header from "@/components/Header";
 import "../styles/custom.scss";
 import LoginForm from "@/components/LoginForm";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Auth = () => {
   return (
     <div>
       <Header />
-      <Container className="my-4">
-        <h2 className="text-center">Welcome to MemoBoard</h2>
-        <h3 className="text-center mb-4">To get started, please sign in</h3>
-        <LoginForm />
+      <Container className="my-5 py-5">
+        <Row className="mb-4">
+          <Col xs={12}>
+            <h2 className="text-center">Welcome to MemoBoard</h2>
+          </Col>
+          <Col xs={12}>
+            <h3 className="text-center">To get started, please sign in</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <LoginForm />
+          </Col>
+        </Row>
       </Container>
     </div>
   );
