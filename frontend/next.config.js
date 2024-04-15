8e6910754fcf1e2766e46eb05638a312a9d4b0f7
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-15 17:14:38 -0500

    Added config files for the new next js project

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       { protocol: "http", hostname: "localhost" },
+//       {
+//         protocol: "https",
+//         hostname: "github.com",
+//         pathname: "/Selenityy/MemoBoard/tree/main/backend/**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
