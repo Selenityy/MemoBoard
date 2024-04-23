821667520ba9aebcc1773743ab8c97d5b0cb4486
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-23 14:23:13 -0500

    Created a dashboard layout that calls header and children

---
+++ frontend/src/app/dashboard/layout.jsx
@@ -0,0 +1,20 @@
+import Header from "@/components/Header";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col className="w-full m-0 p-0">
+          <Header />
+        </Col>
+      </Row>
+      <Row>
+        <Col>{children}</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default DashboardLayout;
