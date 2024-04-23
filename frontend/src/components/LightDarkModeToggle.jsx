b89cec52118ffe4054fbea536e2985ad1f63cebd
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-23 14:22:14 -0500

    Imported useTheme and assigned the toggleTheme to the onChange and the checked theme

--- frontend/src/components/LightDarkModeToggle.jsx
+++ frontend/src/components/LightDarkModeToggle.jsx
@@ -4,9 +4,12 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 import { Col, Container, Row } from "react-bootstrap";
+import { useTheme } from "@/context/ThemeContext";
 import "../styles/custom.scss";
 
 const LightDarkModeToggle = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <Container>
       <Row className="align-items-center">
@@ -17,8 +20,8 @@ const LightDarkModeToggle = () => {
           <Form>
             <Form.Group controlId="dark-mode-switch">
               <Form.Switch
-                //   checked={darkMode}
-                //   onChange={handleToggle}
+                checked={theme === "dark"}
+                onChange={toggleTheme}
                 aria-label="Dark Mode Switch"
               />
             </Form.Group>
