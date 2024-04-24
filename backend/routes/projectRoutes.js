9de22065ede525559374657735558884ab4d9698
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-24 18:33:34 -0500

    Added upate and delete to the router address

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -5,7 +5,7 @@ const projectController = require("../controllers/projectController");
 router.get("/", projectController.listProjects);
 router.get("/:projectId", projectController.getProject);
 router.post("/create", projectController.createProject);
-router.put("/:projectId", projectController.updateProject);
-router.delete("/:projectId", projectController.deleteProject);
+router.put("/:projectId/update", projectController.updateProject);
+router.delete("/:projectId/delete", projectController.deleteProject);
 
 module.exports = router;
