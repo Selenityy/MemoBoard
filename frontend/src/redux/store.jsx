45e2cea4866fba4937a70f188076fea5f75e87aa
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-25 15:23:41 -0500

    Added projectReducer to the store

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
 import memoReducer from "./features/memoSlice";
 import tagReducer from "./features/tagsSlice";
+import projectReducer from "./features/projectSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     memo: memoReducer,
     tag: tagReducer,
+    project: projectReducer,
   },
 });
 
