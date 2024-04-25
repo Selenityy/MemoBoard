34ba06c959163280ab9169d561a843ea6e7ddd24
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-25 15:23:07 -0500

    Wrapped the nav bar buttons in a stack to list the projects below it

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,55 +1,67 @@
 import React from "react";
 import "../styles/custom.scss";
 
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Stack } from "react-bootstrap";
 import LogoutBtn from "./LogoutBtn";
 
 const NavBar = () => {
+
   return (
     <>
       {/* <Container className="nav-bar-contatiner"> */}
-      <Row>
-        <Col>
-          <span>Home</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <span>My Memos</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <span>Today</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <span>This Week</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <hr></hr>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Row className="align-items-center">
-            <Col xs={8}>
-              <span>Projects</span>
+      <Stack gap={2}>
+        <Row>
+          <Col>
+            <span>Home</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <span>My Memos</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <span>Today</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <span>This Week</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <hr></hr>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row className="align-items-center">
+              <Col xs={8}>
+                <span>Projects</span>
+              </Col>
+              <Col xs="auto">
+                <Button size="sm">+</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        {/* {projects.map((project) => {
+          <Row key={project.id}>
+            <Col>
+              <span>{project.name}</span>
             </Col>
-            <Col xs="auto">
-              <Button size="sm">+</Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LogoutBtn />
-        </Col>
-      </Row>
+          </Row>;
+        })} */}
+      </Stack>
+      <div className="mt-auto">
+        <Row>
+          <Col>
+            <LogoutBtn />
+          </Col>
+        </Row>
+      </div>
       {/* </Container> */}
     </>
   );
