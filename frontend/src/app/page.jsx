fb339bc9163f1a4bb6229fd480cf49c6ae7d94e2
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-24 21:37:41 -0500

    Added a signup link button and hr divs with the or

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,13 @@
-import Header from "@/components/Header";
+import HeaderNoHR from "@/components/HeaderNoHR";
 import "../styles/custom.scss";
 import LoginForm from "@/components/LoginForm";
 import { Col, Container, Row } from "react-bootstrap";
+import SignUpBtn from "@/components/SignUpBtn";
 
 const Auth = () => {
   return (
-    <div>
-      <Header />
+    <div className="login-page">
+      <HeaderNoHR />
       <Container className="my-5 py-5">
         <Row className="mb-4">
           <Col xs={12}>
@@ -14,13 +15,34 @@ const Auth = () => {
           </Col>
           <Col xs={12}>
             <h3 className="text-center sign-in-text">
-              To get started, please sign in
+              To get started, please log in
             </h3>
+          </Col>
+        </Row>
+        <Row className="justify-content-center pb-3">
+          <Col xs="auto">
+            <span style={{ color: "#1f1f1f" }}>Google</span>
+          </Col>
+        </Row>
+        <Row className="align-items-center justify-content-center">
+          <Col xs={5} sm={4} md={3} lg={2}>
+            <div className="div-hr-border"></div>
+          </Col>
+          <Col xs="auto">
+            <span style={{ color: "#1f1f1f" }}>or</span>
+          </Col>
+          <Col xs={5} sm={4} md={3} lg={2}>
+            <div className="div-hr-border"></div>
           </Col>
         </Row>
         <Row>
           <Col>
             <LoginForm />
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs="auto">
+            <SignUpBtn />
           </Col>
         </Row>
       </Container>
