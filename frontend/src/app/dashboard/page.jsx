a68dcbabb6b8be6cc594c754660cfa2b1b5087e3
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-25 15:22:04 -0500

    Removed commented out code

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -5,17 +5,12 @@ import { Col, Row } from "react-bootstrap";
 import { useTheme } from "@/context/ThemeContext";
 import React from "react";
 import NavBar from "@/components/NavBar";
-import Header from "@/components/Header";
 
 const DashboardPage = () => {
   const { theme } = useTheme();
   return (
     <Row>
-      <Col
-        xs={2}
-        // style={{ paddingTop: "1rem", borderRight: "0.5px solid black" }}
-        className="nav-bar-contatiner"
-      >
+      <Col xs={2} className="nav-bar-contatiner">
         <NavBar />
       </Col>
       <Col xs={10} className={theme === "dark" ? "body-dark" : "body-light"}>
