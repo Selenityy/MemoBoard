b56a32f2adf72b183b016ed203e1c01f9d1ea147
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-26 19:46:13 -0500

    Created the project modal form but still needs to add in the result of submitting the form

--- frontend/src/components/ProjectModal.jsx
+++ frontend/src/components/ProjectModal.jsx
@@ -1,10 +1,59 @@
 import Modal from "react-bootstrap/Modal";
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Row, Form } from "react-bootstrap";
 import { useTheme } from "@/context/ThemeContext";
+import { useDispatch } from "react-redux";
 
 const ProjectModal = (props) => {
   const { theme } = useTheme();
+  const dispatch = useDispatch();
+  const [projectDetails, setProjectDetails] = useState({
+    name: "",
+    color: "#C8A2C8",
+    description: "",
+  });
+  const [validated, setValidated] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProjectDetails((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const addProjectSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+    }
+    setValidated(true);
+    try {
+      const res = await dispatch(createProject(projectDetails)).unwrap();
+      console.log("res", res);
+      setProjectDetails({
+        name: "",
+        color: "#C8A2C8",
+        description: "",
+      });
+      setValidated(false);
+      setErrorMessage("");
+    } catch (error) {
+      setErrorMessage(error);
+      setValidated(false);
+      console.error("Server error failed to add project:", error);
+    }
+  };
+
+  const closeModal = async (e) => {
+    e.preventDefault();
+    props.onHide();
+    setProjectDetails({
+      name: "",
+      color: "#C8A2C8",
+      description: "",
+    });
+  };
+
   return (
     <Modal
       {...props}
@@ -18,10 +67,10 @@ const ProjectModal = (props) => {
       </Modal.Header>
       <Modal.Body className={theme === "dark" ? "modal-dark" : "modal-light"}>
         <Container>
-          <Form>
+          <Form noValidate validated={validated}>
             <Row className="justify-content-between">
               <Col xs={9}>
-                <Form.Group className="mb-3" controlId="name.ControlInput">
+                <Form.Group className="mb-3" controlId="formName">
                   <Form.Label>Name:</Form.Label>
                   <Form.Control
                     className={
@@ -30,32 +79,40 @@ const ProjectModal = (props) => {
                         : "project-form-styling-light"
                     }
                     type="text"
+                    name="name"
+                    value={projectDetails.name}
+                    onChange={handleChange}
+                    required
                   />
+                  <Form.Control.Feedback
+                    type="invalid"
+                    className="input-feedback"
+                  >
+                    Please provide a name.
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Col>
               <Col xs="auto">
-                <Form.Group className="mb-3" controlId="color.ControlInput">
-                  <Form.Label htmlFor="exampleColorInput">Color</Form.Label>
+                <Form.Group className="mb-3" controlId="formColor">
+                  <Form.Label>Color</Form.Label>
                   <Form.Control
                     type="color"
-                    id="exampleColorInput"
-                    defaultValue="#563d7c"
+                    name="color"
                     title="Choose your color"
                     className={
                       theme === "dark"
                         ? "project-form-styling-dark"
                         : "project-form-styling-light"
                     }
+                    onChange={handleChange}
+                    value={projectDetails.color}
                   />
                 </Form.Group>
               </Col>
             </Row>
             <Row>
               <Col>
-                <Form.Group
-                  className="mb-3"
-                  controlId="description.ControlInput"
-                >
+                <Form.Group className="mb-3" controlId="formDescription">
                   <Form.Label>Description:</Form.Label>
                   <Form.Control
                     className={
@@ -64,19 +121,25 @@ const ProjectModal = (props) => {
                         : "project-form-styling-light"
                     }
                     as="textarea"
+                    name="description"
                     rows={3}
+                    onChange={handleChange}
+                    value={projectDetails.description}
                   />
                 </Form.Group>
               </Col>
             </Row>
+            <div style={{ color: "red" }}>{errorMessage}</div>
           </Form>
         </Container>
       </Modal.Body>
       <Modal.Footer className={theme === "dark" ? "modal-dark" : "modal-light"}>
-        <Button variant="secondary" onClick={props.onHide}>
+        <Button variant="secondary" onClick={closeModal}>
           Close
         </Button>
-        <Button variant="primary">Save Changes</Button>
+        <Button onClick={addProjectSubmit} variant="primary">
+          Save Changes
+        </Button>
       </Modal.Footer>
     </Modal>
   );
