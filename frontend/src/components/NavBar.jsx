3a3a08777ede00c2389485b36e57dc3410f8f77f
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-27 19:06:45 -0500

    Reworked the nav bar container styling

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,6 +10,8 @@ import { RiHome2Line } from "react-icons/ri";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { MdToday } from "react-icons/md";
 import { BsCalendarWeek } from "react-icons/bs";
+import { IoTodayOutline } from "react-icons/io5";
+import { BsCalendar4Week } from "react-icons/bs";
 
 const NavBar = () => {
   const dispatch = useDispatch();
@@ -29,87 +31,89 @@ const NavBar = () => {
   }, [dispatch, refreshDataTrigger]);
 
   return (
-    <>
-      {/* <Container className="nav-bar-contatiner"> */}
-      <Stack gap={2}>
-        <Row>
-          <Col className="nav-icon-cols">
-            <RiHome2Line className="nav-icon-home" />
-            <span>Home</span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="nav-icon-cols">
-            <FaRegCheckCircle className="nav-icon-memos" />
-            <span>My Memos</span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="nav-icon-cols">
-            <MdToday className="nav-icon-today" />
-            <span>Today</span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="nav-icon-cols">
-            <BsCalendarWeek className="nav-icon-week" />
-            <span>This Week</span>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <hr></hr>
-          </Col>
-        </Row>
-        {/* PROJECTS */}
-        <Row style={{ height: "100%" }}>
-          <Col xs={12}>
-            <Row className="align-items-center justify-content-between mb-2">
-              <Col xs={8}>
-                <span>Projects</span>
-              </Col>
-              <Col xs="auto">
-                <button
-                  className="add-project-btn"
-                  onClick={() => setModalShow(true)}
-                >
-                  <IoMdAdd size={20} className="me-2" />
-                </button>
-              </Col>
-            </Row>
-            {projects.map((project) => (
-              <Row key={project._id}>
-                <Col>
-                  <ul className="ps-3 mb-3">
-                    <li className="p-0">
-                      <div
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                        }}
-                      >
-                        <div
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            borderRadius: "7px",
-                            backgroundColor: project.color,
-                            marginRight: "10px",
-                          }}
-                        ></div>
-                        <span>{project.name}</span>
-                      </div>
-                    </li>
-                  </ul>
-                </Col>
-              </Row>
-            ))}
-          </Col>
-        </Row>
+    <Container fluid style={{ padding: "0px" }}>
+      <Row className="align-items-center nav-icon-row">
+        <Col xs="auto">
+          <RiHome2Line className="nav-icon-home" />
+        </Col>
+        <Col className="nav-bar-titles">
+          <span>Home</span>
+        </Col>
+      </Row>
+      <Row className="align-items-center nav-icon-row">
+        <Col xs="auto">
+          <FaRegCheckCircle className="nav-icon-memos" />
+        </Col>
+        <Col className="nav-bar-titles">
+          <span>My Memos</span>
+        </Col>
+      </Row>
+      <Row className="align-items-center nav-icon-row">
+        <Col xs="auto">
+          <IoTodayOutline className="nav-icon-today" />
+        </Col>
+        <Col className="nav-bar-titles">
+          <span>Today</span>
+        </Col>
+      </Row>
+      <Row className="align-items-center nav-icon-row">
+        <Col xs="auto">
+          <BsCalendar4Week className="nav-icon-week" />
+        </Col>
+        <Col className="nav-bar-titles">
+          <span>This Week</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="full-width-hr">
+          <hr />
+        </Col>
+      </Row>
+      {/* PROJECTS */}
+      <Row className="justify-content-bewteen align-items-center">
+        <Col className="project-span">
+          <span>Projects</span>
+        </Col>
+        <Col xs="auto">
+          <button
+            className="add-project-btn"
+            onClick={() => setModalShow(true)}
+          >
+            <IoMdAdd size={20} className="me-2" />
+          </button>
+        </Col>
+      </Row>
+      <Stack gap={3}>
+        {projects.map((project) => (
+          <Row key={project._id} className="align-items-center project-rows">
+            <Col>
+              <ul className="ps-1">
+                <li className="p-0">
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: "15px",
+                        height: "15px",
+                        borderRadius: "4px",
+                        backgroundColor: project.color,
+                        marginRight: "10px",
+                      }}
+                    ></div>
+                    <span className="project-names">{project.name}</span>
+                  </div>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        ))}
       </Stack>
       <ProjectModal show={modalShow} onHide={() => setModalShow(false)} />
-      {/* </Container> */}
-    </>
+    </Container>
   );
 };
 
