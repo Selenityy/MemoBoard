a2a69617fada7f5ba5fd5cb0a62fa69f88315b3e
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-02 15:46:35 -0500

    Fixed import to the styles page

--- frontend/src/app/dashboard/project/[slug]/page.jsx
+++ frontend/src/app/dashboard/project/[slug]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "../../styles/main.scss";
+import "../../../../styles/main.scss";
 import { Col, Row } from "react-bootstrap";
 import { useTheme } from "@/context/ThemeContext";
 import React from "react";
