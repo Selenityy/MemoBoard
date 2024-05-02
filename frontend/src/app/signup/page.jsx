9cacf49388c18cdcf8a3c19011ea5917c356f0a1
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-02 13:10:19 -0500

    Added color themeing to the sign up page

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -1,20 +1,35 @@
+"use client";
+
 import React from "react";
 import LoginHeader from "@/components/LoginHeader";
 import { Col, Row, Container } from "react-bootstrap";
 import SignupForm from "@/components/SignupForm";
 import LoginBtn from "@/components/LoginBtn";
+import { useTheme } from "@/context/ThemeContext";
 
 const SignupPage = () => {
+  const { theme } = useTheme();
+
   return (
     <div className="login-page">
       <LoginHeader />
       <Container className="my-5 py-5">
         <Row className="mb-4">
           <Col xs={12}>
-            <h2 className="text-center welcome-text">Welcome to MemoBoard</h2>
+            <h2
+              className={`text-center ${
+                theme === "dark" ? "welcome-text-dark" : "welcome-text-light"
+              }`}
+            >
+              Welcome to MemoBoard
+            </h2>
           </Col>
           <Col xs={12}>
-            <h3 className="text-center sign-in-text">
+            <h3
+              className={`text-center ${
+                theme === "dark" ? "sign-in-text-dark" : "sign-in-text-light"
+              }`}
+            >
               To get started, please sign up
             </h3>
           </Col>
