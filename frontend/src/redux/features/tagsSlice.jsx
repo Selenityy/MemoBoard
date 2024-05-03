7b1480b0c048ed670b68068ea2968ac8bba50297
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-03 15:17:00 -0500

    Added update and delete tag thunks

--- frontend/src/redux/features/tagsSlice.jsx
+++ frontend/src/redux/features/tagsSlice.jsx
@@ -66,8 +66,73 @@ export const createTag = createAsyncThunk(
 );
 
 // UPDATE TAG
+export const updateTag = createAsyncThunk(
+  "/dashboard/tags/update",
+  async ({ tagId, formData }, thunkAPI) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return thunkAPI.rejectWithValue("No token found");
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:3000/dashboard/tags/${tagId}/update`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Failed to update tag");
+      }
+
+      if (data.updatedTag) {
+        return data.updatedTag;
+      } else {
+        return thunkAPI.rejectWithValue({
+          message: data.message,
+          error: data.errors,
+        });
+      }
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 // DELETE TAG
+export const deleteTag = createAsyncThunk(
+  "/dashboard/memos/delete",
+  async (tagId, thunkAPI) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return thunkAPI.rejectWithValue("No token found");
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:3000/dashboard/tags/${tagId}/delete`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Failed to delete tag");
+      }
+      return data.message;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 const initialState = {
   byId: {},
@@ -113,6 +178,40 @@ const tagsSlice = createSlice({
         state.error = null;
       })
       .addCase(createTag.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+
+      // UPDATE A TAG
+      .addCase(updateTag.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(updateTag.fulfilled, (state, action) => {
+        const updateTag = action.payload;
+        state.byId[updateTag._id] = updateTag;
+        if (!state.allIds.includes(updateTag._id)) {
+          state.allIds.push(updateTag._id);
+        }
+        state.status = "succeeded";
+        state.error = null;
+      })
+      .addCase(updateTag.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+
+      // DELETE A TAG
+      .addCase(deleteTag.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(deleteTag.fulfilled, (state, action) => {
+        const tagId = action.meta.arg;
+        delete state.byId[tagId];
+        state.allIds = state.allIds.filter((id) => id !== tagId);
+        state.status = "succeeded";
+        state.error = null;
+      })
+      .addCase(deleteTag.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.payload;
       });
