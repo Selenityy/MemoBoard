7a252b689d96ebc5157bdf34e39e6cad38c6a9be
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-03 15:02:28 -0500

    Added a create tag thunk

--- frontend/src/redux/features/tagsSlice.jsx
+++ frontend/src/redux/features/tagsSlice.jsx
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+// GET ALL TAGS
 export const fetchTags = createAsyncThunk(
   "/dashboard/tags",
   async (_, thunkAPI) => {
@@ -26,6 +27,48 @@ export const fetchTags = createAsyncThunk(
   }
 );
 
+// CREATE TAG
+export const createTag = createAsyncThunk(
+  "/dashboard/tags/create",
+  async ({ tagId, formData }, thunkAPI) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return thunkAPI.rejectWithValue("No token found");
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:3000/dashboard/tags/${tagId}/create`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Failed to create tag");
+      }
+      if (data.newTag) {
+        return data.newTag;
+      } else {
+        return thunkAPI.rejectWithValue({
+          message: data.message,
+          error: data.errors,
+        });
+      }
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// UPDATE TAG
+
+// DELETE TAG
+
 const initialState = {
   byId: {},
   allIds: [],
@@ -38,6 +81,7 @@ const tagsSlice = createSlice({
   initialState,
   extraReducers: (builder) => {
     builder
+      // GET ALL TAGS
       .addCase(fetchTags.pending, (state) => {
         state.status = "loading";
       })
@@ -52,6 +96,23 @@ const tagsSlice = createSlice({
         state.error = null;
       })
       .addCase(fetchTags.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+
+      // CREATE A TAG
+      .addCase(createTag.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(createTag.fulfilled, (state, action) => {
+        const newTag = action.payload;
+        state.byId[newTag._id] = newTag;
+        state.allIds.push(newTag._id);
+
+        state.status = "succeeded";
+        state.error = null;
+      })
+      .addCase(createTag.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.payload;
       });
