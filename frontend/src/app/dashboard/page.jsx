08c8c0372e462f2e9478ff5030749ed9521320a0
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-06 16:33:12 -0500

    Reformatted the useEffect to get user info, and added checks for whether to update timezone or user information

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -10,66 +10,41 @@ import { useSelector, useDispatch } from "react-redux";
 const DashboardPage = () => {
   const { theme } = useTheme();
   const dispatch = useDispatch();
-  const [username, setUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
   const [dateString, setDateString] = useState("");
-  const [selectedTimezone, setSelectedTimezone] = useState({ value: "UTC" });
+  const { user } = useSelector((state) => state.user);
 
   useEffect(() => {
-    const getUserInfo = async () => {
+    const fetchAndFormateUserInfo = async () => {
       try {
-        const res = await dispatch(fetchUserInfo()).unwrap();
-        setUsername(res.username);
-        setFirstName(res.firstName);
-        setSelectedTimezone(res.timezone);
+        await dispatch(fetchUserInfo()).unwrap();
+        const date = new Date();
+        const options = {
+          weekday: "long",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          timeZone: user.timezone,
+          timeZoneName: "short",
+        };
+        const formatter = new Intl.DateTimeFormat("en-US", options);
+        setDateString(formatter.format(date));
+        console.log("Formatted date:", formatter.format(date));
       } catch (error) {
-        console.error("Failed to get the username", error);
+        console.error(error);
       }
     };
-    getUserInfo();
-  }, [dispatch]);
-
-  useEffect(() => {
-    let storedTimezone = localStorage.getItem("timezone");
-
-    if (!storedTimezone) {
-      const updateTimezone = async () => {
-        try {
-          const response = await dispatch(fetchTimeZone());
-          localStorage.setItem("timezone", response.payload);
-          storedTimezone = response.payload;
-        } catch (error) {
-          console.error("Failed to get the timezone", error);
-        }
-      };
-      updateTimezone();
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    const formatDate = () => {
-      const date = new Date();
-      const options = {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-      };
-      const formatter = new Intl.DateTimeFormat("en-US", options);
-      setDateString(formatter.format(date));
-    };
-    formatDate();
-  }, []);
+    fetchAndFormateUserInfo();
+  }, [user.timezone, dispatch]);
 
   return (
     <>
       <Row>
-        <Col>
-          <span>{dateString}</span>
-        </Col>
+        <Col>{dateString && <span>{dateString}</span>}</Col>
       </Row>
       <Row>
         <Col>
-          <span>Good afternoon, {firstName}</span>
+          <span>Good afternoon, {user.firstName}</span>
         </Col>
       </Row>
     </>
