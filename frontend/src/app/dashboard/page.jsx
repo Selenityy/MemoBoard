637d58e70006d99d973ae7f1790ab7bfaa5f293b
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-08 20:26:36 -0500

    Added row styling and columns so the note section

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -47,7 +47,7 @@ const DashboardPage = () => {
         modules: {
           toolbar: toolbarOptions,
         },
-        placeholder: "Your personal notepad...",
+        // placeholder: "Your personal notepad...",
       });
       setEditor(quill);
 
@@ -142,9 +142,18 @@ const DashboardPage = () => {
           </div>
         </Col>
       </Row>
-      <Row style={{ width: "50%" }}>
-        <Col>
-          <span>Notes</span>
+      <Row
+        style={{
+          width: "50%",
+          border: "1px solid grey",
+          borderRadius: "16px",
+          padding: "none",
+        }}
+      >
+        <Col xs={12} style={{ padding: "16px 0px 5px 25px" }}>
+          <span style={{ fontWeight: "bold" }}>Personal Notes</span>
+        </Col>
+        <Col xs={12}>
           <div
             ref={editorRef}
             style={{
