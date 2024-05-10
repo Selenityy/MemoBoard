ab25848c976a462fdfc19b86542a45a30332f1a0
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-10 14:48:52 -0500

    Removed the welcome user and date, and created a new widget for it

---
+++ frontend/src/components/WelcomeComponent.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import "../styles/main.scss";
+import { Col, Container, Row } from "react-bootstrap";
+import { useTheme } from "@/context/ThemeContext";
+import React, { useEffect, useState } from "react";
+import { fetchUserInfo } from "@/redux/features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import useLiveTime from "@/components/UseLiveTime";
+
+const WelcomeComponent = () => {
+  const { theme } = useTheme();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const [dateString, setDateString] = useState("Loading date...");
+  const [timeOfDay, setTimeOfDay] = useState("Loading time of day...");
+  const clock = useLiveTime(user.timezone || "America/Los_Angeles", 1000);
+
+  useEffect(() => {
+    const fetchAndFormateUserInfo = async () => {
+      try {
+        const fetchedUserInfo = user.timezone
+          ? user
+          : await dispatch(fetchUserInfo()).unwrap();
+        const timeZone = fetchedUserInfo.timezone;
+        const userTimeStr = new Date().toLocaleString("en-US", { timeZone });
+        const userTime = new Date(userTimeStr);
+        const options = {
+          weekday: "long",
+          month: "long",
+          day: "numeric",
+        };
+        const formatter = new Intl.DateTimeFormat("en-US", {
+          ...options,
+          timeZone,
+        });
+
+        setDateString(formatter.format(userTime));
+        const hours = userTime.getHours();
+        setTimeOfDay(
+          hours < 12
+            ? "morning"
+            : hours >= 12 && hours < 17
+            ? "afternoon"
+            : "evening"
+        );
+      } catch (error) {
+        console.error("Error fetching or formatting user info:", error);
+      }
+    };
+    fetchAndFormateUserInfo();
+    // }
+  }, [user.timezone, user.firstName, dispatch]);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <span>Home</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>{clock}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>{dateString}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>
+            Good {timeOfDay}, {user.firstName}
+          </span>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default WelcomeComponent;
