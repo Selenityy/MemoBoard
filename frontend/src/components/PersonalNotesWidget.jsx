cb4cac51ad69978d7c086f69067c26e361c936ee
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-10 14:10:49 -0500

    Wrapped the personal notes in a container

--- frontend/src/components/PersonalNotesWidget.jsx
+++ frontend/src/components/PersonalNotesWidget.jsx
@@ -1,26 +1,28 @@
 "use client";
 
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { QuillComponent } from "@/components/Quill";
 
 const PersonalNotesWidget = () => {
   return (
-    <Row
+    <Container
       style={{
-        width: "50%",
+        width: "100%",
         border: "1px solid grey",
         borderRadius: "16px",
         padding: "none",
       }}
     >
-      <Col xs={12} style={{ padding: "16px 0px 5px 25px" }}>
-        <span style={{ fontWeight: "bold" }}>Personal Notes</span>
-      </Col>
-      <Col xs={12} style={{ width: "full", height: "200px" }}>
-        <QuillComponent />
-      </Col>
-    </Row>
+      <Row>
+        <Col xs={12} style={{ padding: "16px 0px 5px 25px" }}>
+          <span style={{ fontWeight: "bold" }}>Personal Notes</span>
+        </Col>
+        <Col xs={12} style={{ width: "full", height: "400px" }}>
+          <QuillComponent />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
