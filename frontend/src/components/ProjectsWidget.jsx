aaad4b9928c856602146f3259f82444c94bc8622
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-14 13:04:10 -0500

    Created the projects widget selector and mapped the projects with some styling

--- frontend/src/components/ProjectsWidget.jsx
+++ frontend/src/components/ProjectsWidget.jsx
@@ -1,9 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import { useTheme } from "@/context/ThemeContext";
+import { useSelector, useDispatch } from "react-redux";
+import { createSelector } from "reselect";
+import { fetchProjects } from "@/redux/features/projectSlice";
+
+const selectAllProjects = createSelector(
+  [(state) => state.project.allIds, (state) => state.project.byId],
+  (allIds, byId) => allIds.map((id) => byId[id])
+);
 
 const ProjectsWidget = () => {
+  const { theme } = useTheme();
+  const dispatch = useDispatch();
+  const allProjects = useSelector(selectAllProjects);
+
+  useEffect(() => {
+    dispatch(fetchProjects());
+  }, [dispatch]);
+
   return (
     <Container
       style={{
@@ -19,18 +36,63 @@ const ProjectsWidget = () => {
         style={{
           marginLeft: "-16px",
           marginRight: "-16px",
-          borderBottom: "1px solid grey",
+          borderBottom: "none",
           paddingLeft: "4px",
           paddingRight: "4px",
         }}
       >
         <Col>
-          <span style={{ fontWeight: "bold" }}>Projects</span>
+          <span
+            style={{
+              fontWeight: "bold",
+              fontSize: "1.2rem",
+            }}
+          >
+            Projects
+          </span>
         </Col>
       </Row>
       <Row>
-        <Col>
-          <div>list of projects here</div>
+        <Col
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gridAutoFlow: "row",
+            padding: "20px 30px 30px 30px",
+            gap: "20px",
+          }}
+        >
+          {allProjects.map((project) => (
+            <div
+              key={project._id}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                width: "max-content",
+              }}
+            >
+              <ul
+                style={{
+                  flex: 1,
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <li>
+                  <div
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                      borderRadius: "4px",
+                      backgroundColor: project.color,
+                      marginRight: "10px",
+                    }}
+                  ></div>
+                </li>
+                <li style={{ fontSize: "1rem" }}>{project.name}</li>
+              </ul>
+            </div>
+          ))}
         </Col>
       </Row>
     </Container>
