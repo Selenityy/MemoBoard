de9e8cbdd7b42bf3812cb0570cbc5a6821d1c4b1
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-05-22 12:29:59 -0500

    Added createSelector and utilized the redux persist to maintain the different project slugs

--- frontend/src/app/dashboard/project/[slug]/page.jsx
+++ frontend/src/app/dashboard/project/[slug]/page.jsx
@@ -1,17 +1,79 @@
 "use client";
 
 import "../../../../styles/main.scss";
-import { Col, Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import React, { useEffect } from "react";
 import { useTheme } from "@/context/ThemeContext";
-import React from "react";
+import { createSelector } from "reselect";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProject, fetchProjects } from "@/redux/features/projectSlice";
+import ProjectPageComponent from "@/components/ProjectPageComponent";
+
+const makeGetProjectByDisplayName = () => {
+  return createSelector(
+    [
+      (state) => state.project.allIds,
+      (state) => state.project.byId,
+      (_, displayName) => displayName,
+    ],
+    (allIds, byId, displayName) => {
+      return allIds
+        .map((id) => byId[id])
+        .find((project) => project.name === displayName);
+    }
+  );
+};
 
 const ProjectPage = ({ params: { slug } }) => {
   const { theme } = useTheme();
+  const dispatch = useDispatch();
   const displayName = slug.replace(/-/g, " ");
+  const getProjectByDisplayName = makeGetProjectByDisplayName();
+  const project = useSelector((state) =>
+    getProjectByDisplayName(state, displayName)
+  );
+
+  useEffect(() => {
+    if (!project) {
+      dispatch(fetchProjects());
+    }
+  }, [dispatch, project]);
+
   return (
-    <div>
-      <span>Project Page for {displayName}</span>
-    </div>
+    <>
+      <Row>
+        <Col>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "25px",
+                height: "25px",
+                borderRadius: "4px",
+                backgroundColor: project.color,
+                marginRight: "10px",
+              }}
+            ></div>
+            <span
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: "700",
+              }}
+              className={
+                theme === "dark" ? "project-names-dark" : "project-names-light"
+              }
+            >
+              {displayName}
+            </span>
+          </div>
+        </Col>
+      </Row>
+      <ProjectPageComponent project={project} />
+    </>
   );
 };
 
