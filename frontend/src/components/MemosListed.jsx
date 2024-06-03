7508eb5ef4be133004f3778f29335c48f053fa2a
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-06-03 15:23:57 -0500

    Moved the modal to it's own component with a list of params that call the functions and values within this parent

--- frontend/src/components/MemosListed.jsx
+++ frontend/src/components/MemosListed.jsx
@@ -23,6 +23,7 @@ import { CiCalendar } from "react-icons/ci";
 import "react-calendar/dist/Calendar.css";
 import Modal from "react-bootstrap/Modal";
 import Select from "react-dropdown-select";
+import MemoDetailsModal from "./MemoDetailsModal";
 
 const allProjects = createSelector(
   [(state) => state.project.allIds, (state) => state.project.byId],
@@ -481,324 +482,39 @@ const MemosListed = ({ project }) => {
   return (
     <>
       {showMemoModal && selectedMemo && (
-        <Modal
-          show={showMemoModal}
-          onHide={handleClose}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title
-              style={{
-                fontSize: "0.5rem",
-                display: "flex",
-                width: "100%",
-                justifyContent: "space-between",
-                alignItems: "center",
-                paddingRight: "10px",
-              }}
-            >
-              {selectedMemo.progress !== "Completed" ? (
-                <>
-                  <button
-                    onClick={() =>
-                      toggleMemoProgress(selectedMemo, selectedMemo._id)
-                    }
-                  >
-                    &#10003; Mark Complete
-                  </button>
-                  <div
-                    onClick={clickEllipsis}
-                    style={{ color: "black", fontSize: "1rem" }}
-                  >
-                    ...
-                  </div>
-                </>
-              ) : (
-                <>
-                  <button
-                    onClick={() =>
-                      toggleMemoProgress(selectedMemo, selectedMemo._id)
-                    }
-                    style={{ backgroundColor: "green" }}
-                  >
-                    &#10003; Completed
-                  </button>
-                  <div
-                    onClick={clickEllipsis}
-                    style={{ color: "black", fontSize: "1rem" }}
-                  >
-                    ...
-                  </div>
-                </>
-              )}
-              {showEllipsis && (
-                <div
-                  onClick={() => clickDeleteMemo(selectedMemo)}
-                  style={{ color: "black" }}
-                >
-                  Delete
-                </div>
-              )}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Container>
-              <Row>
-                <Col>
-                  {selectedMemo.parentId && (
-                    <div
-                      onClick={() => toggleMemoModal(selectedMemo.parentId)}
-                      style={{ color: "black" }}
-                    >
-                      {selectedMemo.parentId.body}
-                    </div>
-                  )}
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <textarea
-                    aria-label="Memo Body"
-                    style={{
-                      width: "100%",
-                      height: "40px",
-                      overflow: "hidden",
-                      resize: "none",
-                      padding: "10px",
-                      border: "none",
-                    }}
-                    value={memoBody}
-                    onChange={(e) => setMemoBody(e.target.value)}
-                    onBlur={() => updateBody(selectedMemo)}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter") {
-                        updateBody(selectedMemo);
-                      }
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col style={{ padding: "10px 20px 10px 20px" }}>
-                  <div style={{ color: "black" }}>Due Date</div>
-                </Col>
-                <Col>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "10px",
-                    }}
-                  >
-                    {selectedMemo.dueDateTime ? (
-                      <>
-                        <div
-                          style={{ color: "black", cursor: "pointer" }}
-                          onClick={() => toggleBigCalendar(selectedMemo._id)}
-                        >
-                          {format(parseISO(selectedMemo.dueDateTime), "MMM d")}
-                        </div>
-                        <div
-                          style={{
-                            color: "black",
-                            fontSize: "1rem",
-                            cursor: "pointer",
-                          }}
-                          onClick={() => clearDueDate(selectedMemo)}
-                        >
-                          X
-                        </div>
-                      </>
-                    ) : (
-                      <CiCalendar
-                        style={{ color: "black" }}
-                        onClick={() => toggleBigCalendar(selectedMemo._id)}
-                      />
-                    )}
-                  </div>
-                  <div
-                    ref={(el) => (calendarRefs.current[selectedMemo._id] = el)}
-                    style={{ position: "relative" }}
-                  >
-                    {showBigCalendar[selectedMemo._id] && (
-                      <div
-                        style={{
-                          position: "absolute",
-                          zIndex: 1050,
-                          top: "100%",
-                          left: 0,
-                        }}
-                      >
-                        <Calendar
-                          onChange={(date) => {
-                            changeDueDate(date, selectedMemo);
-                            toggleBigCalendar(selectedMemo._id);
-                          }}
-                          value={
-                            selectedMemo.dueDateTime
-                              ? parseISO(selectedMemo.dueDateTime)
-                              : null
-                          }
-                          calendarType={"gregory"}
-                          style={{ backgroundColor: "white", color: "black" }}
-                        />
-                      </div>
-                    )}
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <div style={{ color: "black" }}>Progress</div>
-                </Col>
-                <Col>
-                  <Select
-                    style={{ color: "black" }}
-                    options={options}
-                    values={[memoProgress]}
-                    onChange={(selectedOption) => {
-                      updateProgress(selectedOption);
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <div style={{ color: "black" }}>Projects</div>
-                </Col>
-                <Col>
-                  <Select
-                    clearable
-                    style={{ color: "black" }}
-                    options={projectOptions}
-                    values={memoProjects}
-                    onChange={(selectedOption) => {
-                      updateProject(selectedOption);
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <div style={{ color: "black" }}>Descriptions</div>
-                </Col>
-                <Col>
-                  <textarea
-                    style={{ color: "black" }}
-                    placeholder="What is this memo about?"
-                    value={memoNotes}
-                    onChange={(e) => setMemoNotes(e.target.value)}
-                    onBlur={() => updateNotes(selectedMemo)}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter") {
-                        updateNotes(selectedMemo);
-                      }
-                    }}
-                  ></textarea>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <button onClick={handleSubMemoAddClick}>+ Add submemo</button>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  {newSubMemoLine && (
-                    <Row>
-                      <Col>
-                        <input
-                          ref={submemoRef}
-                          type="text"
-                          onChange={(e) => setNewSubMemoText(e.target.value)}
-                          placeholder="Type new submemo here..."
-                          className="form-control"
-                          onBlur={() => createSubMemoClick(selectedMemo)}
-                        />
-                      </Col>
-                    </Row>
-                  )}
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <ul>
-                    {submemos.length > 0 &&
-                      submemos.map((submemo) => (
-                        <li
-                          key={submemo._id}
-                          style={{ listStyleType: "circle", color: "black" }}
-                          onClick={() => toggleMemoModal(submemo)}
-                        >
-                          {submemo.progress === "Completed" ? (
-                            <>
-                              <span
-                                style={{
-                                  color: "grey",
-                                  textDecoration: "line-through",
-                                }}
-                              >
-                                {submemo.body}
-                              </span>
-                              <span
-                                style={{
-                                  cursor: submemo.dueDateTime
-                                    ? "pointer"
-                                    : "default",
-                                  color: "grey",
-                                  textDecoration: "line-through",
-                                }}
-                              >
-                                {submemo.dueDateTime
-                                  ? isToday(parseISO(submemo.dueDateTime))
-                                    ? "Today"
-                                    : format(
-                                        parseISO(submemo.dueDateTime),
-                                        "MMM d"
-                                      )
-                                  : null}
-                              </span>
-                            </>
-                          ) : (
-                            <>
-                              <span style={{ color: "black" }}>
-                                {submemo.body}
-                              </span>
-                              {submemo.dueDateTime !== null && (
-                                <span
-                                  style={{
-                                    cursor: submemo.dueDateTime
-                                      ? "pointer"
-                                      : "default",
-                                    color: isToday(
-                                      parseISO(submemo.dueDateTime)
-                                    )
-                                      ? "green"
-                                      : "black",
-                                  }}
-                                >
-                                  {submemo.dueDateTime
-                                    ? isToday(parseISO(submemo.dueDateTime))
-                                      ? "Today"
-                                      : format(
-                                          parseISO(submemo.dueDateTime),
-                                          "MMM d"
-                                        )
-                                    : null}
-                                </span>
-                              )}
-                            </>
-                          )}
-                        </li>
-                      ))}
-                  </ul>
-                </Col>
-              </Row>
-            </Container>
-          </Modal.Body>
-        </Modal>
+        <MemoDetailsModal
+          selectedMemo={selectedMemo}
+          showMemoModal={showMemoModal}
+          handleClose={handleClose}
+          toggleMemoProgress={toggleMemoProgress}
+          clickEllipsis={clickEllipsis}
+          showEllipsis={showEllipsis}
+          clickDeleteMemo={clickDeleteMemo}
+          toggleMemoModal={toggleMemoModal}
+          setMemoBody={setMemoBody}
+          updateBody={updateBody}
+          toggleBigCalendar={toggleBigCalendar}
+          showBigCalendar={showBigCalendar}
+          clearDueDate={clearDueDate}
+          calendarRefs={calendarRefs}
+          changeDueDate={changeDueDate}
+          updateProgress={updateProgress}
+          projectOptions={projectOptions}
+          memoProjects={memoProjects}
+          updateProject={updateProject}
+          memoNotes={memoNotes}
+          memoBody={memoBody}
+          setMemoNotes={setMemoNotes}
+          updateNotes={updateNotes}
+          options={options}
+          memoProgress={memoProgress}
+          handleSubMemoAddClick={handleSubMemoAddClick}
+          newSubMemoLine={newSubMemoLine}
+          submemoRef={submemoRef}
+          setNewSubMemoText={setNewSubMemoText}
+          submemos={submemos}
+          createSubMemoClick={createSubMemoClick}
+        />
       )}
       <ul>
         {projectMemos.map((memo) => (
