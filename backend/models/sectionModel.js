a6cbabee031e811900bfb8eef4a55d4fbf7b6011
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-06-10 14:56:17 -0500

    Added user to the section schema

--- backend/models/sectionModel.js
+++ backend/models/sectionModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const SectionSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true, maxLength: 100 },
   memos: [{ type: Schema.Types.ObjectId, ref: "Memo" }],
   index: { type: Number, required: true, default: 0 },
