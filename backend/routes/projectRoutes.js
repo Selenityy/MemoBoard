db8a547160852235ea13df987e48535af049a792
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-06-11 11:45:33 -0500

    Added passport and sectoinRoutes to the imports

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const projectController = require("../controllers/projectController");
+const sectionRoutes = require("../routes/sectonRoutes");
 
 router.get("/", projectController.listProjects);
 router.get("/:projectId", projectController.getProject);
