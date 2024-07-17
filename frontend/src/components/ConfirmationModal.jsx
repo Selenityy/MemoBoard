0d68c7926b8b28f2e48d41f037af28237b18acbd
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-07-17 14:12:00 -0500

    Created a confirmation modal for account deletion

---
+++ frontend/src/components/ConfirmationModal.jsx
@@ -0,0 +1,22 @@
+import { Modal, Button } from "react-bootstrap";
+
+const ConfirmationModal = ({ show, handleClose, handleConfirm, message }) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title style={{ color: "black" }}>Confirm Action</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ color: "black" }}>{message}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleConfirm}>
+          Confirm
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
