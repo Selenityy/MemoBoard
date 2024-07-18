fe163008f8eb6966716353c19a54d376c7052cd3
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-07-18 14:40:27 -0500

    Added the user personal notes as the notes value, changed the useEffect to update it as well as the handleChange to use the updateUserNotes in the slice

--- frontend/src/components/Quill.jsx
+++ frontend/src/components/Quill.jsx
@@ -3,21 +3,26 @@
 import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useDispatch } from "react-redux";
+import { updateUserNotes } from "@/redux/features/userSlice";
+import { useCallback } from "react";
+import debounce from "lodash/debounce";
 
-export const QuillComponent = () => {
-  const [value, setValue] = useState("");
+export const QuillComponent = ({ userPersonalNotes }) => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState(userPersonalNotes);
+  console.log("notes:", value);
 
   useEffect(() => {
-    const storedNotes = localStorage.getItem("notes");
-    if (storedNotes) {
-      setValue(storedNotes);
-    }
-  }, []);
+    setValue(userPersonalNotes);
+  }, [userPersonalNotes]);
 
-  const handleChange = (content) => {
-    localStorage.setItem("notes", content);
-    setValue(content);
-  };
+  const handleChange = useCallback(
+    debounce((content) => {
+      dispatch(updateUserNotes(content));
+    }, 400),
+    [dispatch]
+  );
 
   return <ReactQuill theme="snow" value={value} onChange={handleChange} />;
 };
