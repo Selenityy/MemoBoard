16e6bc67a4ce1f96d17b1c1113159034ada49e15
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-07-24 13:25:10 -0500

    Changed the fetching of a project to use the id, created a split slug function, and make the project name and description editable

--- frontend/src/app/dashboard/project/[slug]/page.jsx
+++ frontend/src/app/dashboard/project/[slug]/page.jsx
@@ -2,43 +2,108 @@
 
 import "../../../../styles/main.scss";
 import { Row, Col } from "react-bootstrap";
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import { useTheme } from "@/context/ThemeContext";
 import { createSelector } from "reselect";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchProject, fetchProjects } from "@/redux/features/projectSlice";
+import {
+  fetchProject,
+  fetchProjects,
+  updateProject,
+} from "@/redux/features/projectSlice";
+import { debounce } from "lodash";
+import { useRouter } from "next/navigation";
 import ProjectPageSections from "@/components/ProjectPageSections";
+import ContentEditable from "react-contenteditable";
 
-const makeGetProjectByDisplayName = () => {
-  return createSelector(
-    [
-      (state) => state.project.allIds,
-      (state) => state.project.byId,
-      (_, displayName) => displayName,
-    ],
-    (allIds, byId, displayName) => {
-      return allIds
-        .map((id) => byId[id])
-        .find((project) => project.name === displayName);
-    }
-  );
+const getProjectById = createSelector(
+  [(state) => state.project.byId, (_, projectId) => projectId],
+  (byId, projectId) => byId[projectId]
+);
+
+const splitSlug = (slug) => {
+  const lastIndex = slug.lastIndexOf("-");
+  const firstPart = slug.substring(0, lastIndex);
+  const secondPart = slug.substring(lastIndex + 1);
+
+  return { firstPart, secondPart };
 };
 
-const ProjectPage = ({ params: { slug } }) => {
-  console.log(slug);
+const ProjectPage = ({ params }) => {
   const { theme } = useTheme();
   const dispatch = useDispatch();
-  const displayName = slug.replace(/-/g, " ");
-  const getProjectByDisplayName = makeGetProjectByDisplayName();
-  const project = useSelector((state) =>
-    getProjectByDisplayName(state, displayName)
+  const { slug } = params;
+  const { secondPart: projectId } = splitSlug(slug);
+  const router = useRouter();
+  console.log("query", router.query);
+  const project = useSelector((state) => getProjectById(state, projectId));
+  const initialDisplayName = params.slug.replace(/-/g, " ");
+  const [projectName, setProjectName] = useState(initialDisplayName);
+  const [projectDescription, setProjectDescription] = useState(
+    project?.description || ""
   );
+  const projectNameRef = useRef(null);
+  const projectDescriptionRef = useRef(null);
+  const [userChanged, setUserChanged] = useState(false);
+
+  // console.log("params:", params);
+  // console.log("project:", project);
+  // console.log("project name:", projectName);
+  // console.log("display name:", displayName);
+  // console.log("description:", projectDescription);
+  // console.log("project id:", projectId);
 
   useEffect(() => {
     if (!project) {
       dispatch(fetchProjects());
+    } else if (project && !userChanged) {
+      const { firstPart, secondPart } = splitSlug(params.slug);
+      console.log("first part:", firstPart);
+      setProjectName(firstPart.replace(/-/g, " "));
+      setProjectDescription(project.description || "");
     }
-  }, [dispatch, project]);
+  }, [dispatch, project, params.slug]);
+
+  const handleProjectNameChange = useCallback(
+    debounce(async (e) => {
+      const newProjectName = e.target.value;
+      const displayNameEdited = newProjectName.replace(/-/g, " ");
+      console.log("edited:", displayNameEdited);
+      setProjectName(displayNameEdited);
+      try {
+        await dispatch(
+          updateProject({
+            projectId: project._id,
+            projectData: { name: displayNameEdited },
+          })
+        );
+        const newSlug = newProjectName.replace(/ /g, "-");
+        console.log("new slug:", newSlug);
+        router.push(`/dashboard/project/${newSlug}-${projectId}`);
+      } catch (error) {
+        console.error("Error updating the project name:", error);
+      }
+    }, 500),
+    [dispatch, projectName, router]
+  );
+
+  const handleProjectDescriptionChange = useCallback(
+    debounce(async (e) => {
+      const newProjectDescription = e.target.value;
+      setProjectDescription(newProjectDescription);
+      try {
+        await dispatch(
+          updateProject({
+            projectId: project._id,
+            projectData: { description: newProjectDescription },
+          })
+        );
+      } catch (error) {
+        console.error("Error updating the project description:", error);
+      }
+    }, 500),
+    [dispatch, projectDescription]
+  );
 
   return (
     <>
@@ -59,7 +124,11 @@ const ProjectPage = ({ params: { slug } }) => {
                 marginRight: "10px",
               }}
             ></div>
-            <span
+            <ContentEditable
+              innerRef={projectNameRef}
+              html={projectName}
+              onChange={(e) => handleProjectNameChange(e)}
+              tagName="div"
               style={{
                 fontSize: "1.5rem",
                 fontWeight: "700",
@@ -67,14 +136,19 @@ const ProjectPage = ({ params: { slug } }) => {
               className={
                 theme === "dark" ? "project-names-dark" : "project-names-light"
               }
-            >
-              {displayName}
-            </span>
+            />
           </div>
         </Col>
       </Row>
       <Row>
-        <Col>{project.description}</Col>
+        <Col>
+          <ContentEditable
+            innerRef={projectDescriptionRef}
+            html={projectDescription}
+            onChange={(e) => handleProjectDescriptionChange(e)}
+            tagName="div"
+          />
+        </Col>
       </Row>
       <ProjectPageSections project={project} />
     </>
