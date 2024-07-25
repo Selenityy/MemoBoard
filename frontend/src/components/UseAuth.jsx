077ba0dbcd762de36f41ffc6eb3f198f3f16862d
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-07-25 14:40:26 -0500

    Added a useAuth to ensure only authenticated users can access pages beyond the signup/login

---
+++ frontend/src/components/UseAuth.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+
+const useAuth = () => {
+  const router = useRouter();
+  const isAuthenticated = useSelector((state) => state.user._id);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/");
+    }
+  }, [isAuthenticated, router]);
+
+  return isAuthenticated;
+};
+
+export default useAuth;
