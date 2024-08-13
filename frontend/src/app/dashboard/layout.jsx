7dbe521ee29ce141bdf75ec091a011c29bff1b29
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-08-12 20:21:37 -0500

    Added the mobile nav bar for screens small and smaller, added display none to the appropriate screens

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useTheme } from "@/context/ThemeContext";
 import useAuth from "@/components/UseAuth";
+import MobileNavBar from "@/components/MobileNavBar";
 
 const DashboardLayout = ({ children }) => {
   const { theme } = useTheme();
@@ -17,23 +18,24 @@ const DashboardLayout = ({ children }) => {
   return (
     <Container
       fluid
-      style={{ width: "full", height: "100vh", overflowY: "scroll" }}
+      style={{ width: "100%", height: "100vh", overflowY: "scroll" }}
+      className="container-fluid"
     >
       <Row>
         <Col className="w-full m-0 p-0">
           <Header />
         </Col>
       </Row>
-      <Row>
+      <Row className="min-vh-100 d-none d-md-flex">
         <Col
           xs={2}
           className={`${
             theme === "dark"
-              ? "nav-bar-contatiner-dark"
-              : "nav-bar-contatiner-light"
+              ? "nav-bar-container-dark"
+              : "nav-bar-container-light"
           }`}
         >
-          <NavBar />
+          <NavBar className="pe-2" />
         </Col>
         <Col
           className={`w-100% px-4 pt-3 ${
@@ -42,6 +44,44 @@ const DashboardLayout = ({ children }) => {
           style={{ overflowX: "auto" }}
         >
           {children}
+        </Col>
+      </Row>
+      {/* Mobile version */}
+      <Row className="min-vh-100 d-flex d-md-none">
+        {/* <Col
+          xs={2}
+          className={`${
+            theme === "dark"
+              ? "mobile-nav-bar-container-dark"
+              : "mobile-nav-bar-container-light"
+          }`}
+        >
+          <MobileNavBar className="pe-2" />
+        </Col> */}
+        <Col
+          className={`w-100% px-4 pt-3 ${
+            theme === "dark" ? "body-dark" : "body-light"
+          }`}
+          style={{ overflowX: "auto" }}
+        >
+          {children}
+        </Col>
+      </Row>
+      <Row
+        className={`${
+          theme === "dark"
+            ? "mobile-nav-bar-row-dark"
+            : "mobile-nav-bar-row-light"
+        } d-md-none`}
+      >
+        <Col
+          className={`${
+            theme === "dark"
+              ? "mobile-nav-bar-col-dark"
+              : "mobile-nav-bar-col-light"
+          } p-0`}
+        >
+          <MobileNavBar />
         </Col>
       </Row>
     </Container>
